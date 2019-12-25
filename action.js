699b2e210d87f8094875ca8e4167e6fb172e81c7
SondreB <sondre@outlook.com>
2019-12-25 22:16:02 +0100

    Update action.js
    Install the changelog

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 require('child_process')
     .execSync(
-        'npm install @actions/core @actions/github',
+        'npm install @actions/core @actions/github conventional-changelog-cli',
         { cwd: __dirname }
     );  
 const fs = require('fs');
